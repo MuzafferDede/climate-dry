b20c5ffc36bff8dae0e5a37b43195e23ef93b1e4
binarywebs <contact@binarywebs.co.uk>
2025-07-23 17:45:02 +0800

    live chat added

--- app/root.tsx
+++ app/root.tsx
@@ -126,6 +126,24 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				{children}
 				<ScrollRestoration />
 				<Scripts />
+
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+					var __lc = {};
+					__lc.license = 1215771;
+					__lc.skill = 41;
+
+					(function() {
+						var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
+						lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
+						var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
+					})();
+						`,
+					}}
+				/>
+
+
 			</body>
 		</html>
 	);
