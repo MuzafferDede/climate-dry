1f788c6b1c8457ca1936b7cc391cecafc52da10a
binarywebs <contact@binarywebs.co.uk>
2025-07-29 22:21:49 +0800

    Blog h1 duplicate fix

--- app/components/blog-posts/intro-text.tsx
+++ app/components/blog-posts/intro-text.tsx
@@ -2,7 +2,7 @@ export const BlogIntroText = () => {
 	return (
 		<div>
 			<div className="space-y-2">
-				<h1 className="font-bold text-4xl text-gray-darkest">Advice Hub</h1>
+				<h2 className="font-bold text-4xl text-gray-darkest">Advice Hub</h2>
 			</div>
 			<div className="prose prose-img:mx-auto prose-figcaption:hidden max-w-none prose-img:max-w-full">
 				<p>
