879c6b13ce5202133337883a538296f1344a1dec
binarywebs <contact@binarywebs.co.uk>
2025-08-06 15:14:17 +0800

    Update vite.config.ts (#63)

--- vite.config.ts
+++ vite.config.ts
@@ -1,48 +1,43 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { iconsSpritesheet } from "vite-plugin-icons-spritesheet";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ isSsrBuild }) => ({
-	server: {
-		proxy: {
-			"/s3": {
-				target: `${process.env.BACKEND_URL}/storage`,
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/s3/, ""),
+export default defineConfig(({ isSsrBuild, mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+
+	return {
+		server: {
+			proxy: {
+				"/s3": {
+					target: `${env.BACKEND_URL}/storage`,
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/s3/, ""),
+				},
 			},
 		},
-	},
-	build: {
-		rollupOptions: isSsrBuild
-			? {
-					input: "./server/app.ts",
-				}
-			: undefined,
-	},
-	plugins: [
-		tailwindcss(),
-		reactRouter(),
-		tsconfigPaths(),
-		iconsSpritesheet({
-			// Defaults to false, should it generate TS types for you
-			withTypes: true,
-			// The path to the icon directory
-			inputDir: "app/assets/icons",
-			// Output path for the generated spritesheet and types
-			outputDir: "app/assets",
-			// Output path for the generated type file, defaults to types.ts in outputDir
-			typesOutputFile: "app/types/icons.ts",
-			// The name of the generated spritesheet, defaults to sprite.svg
-			fileName: "icon.svg",
-			// The cwd, defaults to process.cwd()
-			cwd: process.cwd(),
-			// What formatter to use to format the generated files, prettier or biome, defaults to no formatter
-			formatter: "biome",
-			// Callback function that is called when the script is generating the icon name
-			// This is useful if you want to modify the icon name before it is written to the file
-			iconNameTransformer: (iconName) => iconName,
-		}),
-	],
-}));
+		build: {
+			rollupOptions: isSsrBuild
+				? {
+						input: "./server/app.ts",
+					}
+				: undefined,
+		},
+		plugins: [
+			tailwindcss(),
+			reactRouter(),
+			tsconfigPaths(),
+			iconsSpritesheet({
+				withTypes: true,
+				inputDir: "app/assets/icons",
+				outputDir: "app/assets",
+				typesOutputFile: "app/types/icons.ts",
+				fileName: "icon.svg",
+				cwd: process.cwd(),
+				formatter: "biome",
+				iconNameTransformer: (iconName) => iconName,
+			}),
+		],
+	};
+});
